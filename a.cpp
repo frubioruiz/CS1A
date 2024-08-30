35f97c8af187d138c15e70bedb01d3f812d5e051
frubioruiz <frubioruiz0@saddleback.edu>
2024-08-30 02:38:36 +0000

    rack up those fryers.

---
+++ a.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main () {
+    // here is where our code is going to go
+    // a line starting with // means it is a comment for humans. this way we ask the compiler to not run these lines.
+    /* this is 
+       how 
+       we cam wrote 
+       multiple lines for 
+       comments
+    */
+   // how to write something in the output? cout stands for console out
+   // every line of codes end with ;
+   // literal strings are enclosed in " "
+   // in the following line cout is not enclosed in "" and thus the compiler will know that
+   // cout is a c++ instruction.
+    cout <<"We have started to learn about coding!";
+    // count << "\n" will go to the next line.
+    cout <<"\n\n\n";
+}
