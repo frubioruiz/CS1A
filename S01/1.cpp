6fa13b7440488a06950f9f2657d93aaa344020dc
frubioruiz <frubioruiz0@saddleback.edu>
2024-08-30 03:49:22 +0000

    mistakes were made

---
+++ S01/1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main () {
+    cout <<"Francisco";
+    cout <<"\nRubio-Ruiz";
+    cout <<"\n18 years old";
+}
---
+++ b.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main () {
+    cout <<"\n Congratulations! You just signed your life away to start learning Computer Science \n";
+}
